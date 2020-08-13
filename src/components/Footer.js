bd3033cde80534ee42146912154c124991de4bee
Rudy Malhi <rudy.malhi@sage.com>
2020-08-13 20:37:26 +0300

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import './Footer.css'
 
 export default () => (
   <div>
     <footer className="footer">
       <div className="container taCenter">
-        <span>עמותת יהל 054-4671019</span><a class="social-link" href="https://www.facebook.com/Foodallergy.il/"><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&amp;h=512"></img></a>
+        <span>עמותת יהל 054-4671019</span>
+        <a class="social-link" href="https://www.facebook.com/Foodallergy.il/"><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&amp;h=512"></img></a>
+        <Link to="/toc/">תקנון האתר</Link>
       </div>
     </footer>
   </div>
