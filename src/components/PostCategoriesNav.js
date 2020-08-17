addf74eebc14d1eacbe9d3907e86781a9557b91a
Rudy Malhi <rudy.malhi@sage.com>
2020-08-17 12:12:14 +0300

    .

--- src/components/PostCategoriesNav.js
+++ src/components/PostCategoriesNav.js
@@ -7,7 +7,7 @@ import './PostCategoriesNav.css'
 const PostCategoriesNav = ({ categories, enableSearch }) => (
   <div className="PostCategoriesNav">
     <Link className="NavLink" exact="true" to={`/blog/`}>
-      הכל
+    כל המאמרים
     </Link>
     {categories.map((category, index) => (
       <Link
