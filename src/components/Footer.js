7b0477768a3471059a3b8e8984a27c75a4170465
Rudy Malhi <rudy.malhi@sage.com>
2020-11-30 17:59:06 +0200

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
   <div>
     <footer className="footer">
       <div className="container taCenter">
-        <span>עמותת יהל 054-4671019</span>
+        <span>עמותת יהל ע“ר 580487569 טל. 054-4671019</span>
         <a class="social-link" href="https://www.facebook.com/Foodallergy.il/"><img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&amp;h=512"></img></a>
         <Link to="/toc/">תקנון האתר</Link>
       </div>
